f131994e7e0665ce322b7ca55fb781a079e3ca8a
pandejaya33 <pandejaya33@gmail.com>
2026-01-27 07:24:16 +0900

    Update room.js

--- room.js
+++ room.js
@@ -1,7 +1,7 @@
 import { db } from "./firebase.js";
 import { doc, setDoc, updateDoc, onSnapshot, collection } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { buatDeck } from "./game.js";
+import { buatDeck, hitungPoint } from "./game.js";
 
 const roomId = localStorage.getItem("roomId");
 const name = localStorage.getItem("playerName");
@@ -14,6 +14,25 @@ await setDoc(doc(db,"rooms",roomId,"players",playerId),{
   ready:false,
   cards:[]
 });
+const roomRef = doc(db,"rooms",roomId);
+
+onSnapshot(roomRef, async (roomSnap)=>{
+  const room = roomSnap.data();
+  if(!room) return;
+
+  if(room.botCount && !room.botsCreated){
+    for(let i=1;i<=room.botCount;i++){
+      await setDoc(doc(db,"rooms",roomId,"players","bot"+i),{
+        name:"Bot "+i,
+        ready:true,
+        isBot:true,
+        cards:[]
+      });
+    }
+    await updateDoc(roomRef,{botsCreated:true});
+  }
+});
+
 
 window.readyUp = async function(){
   await updateDoc(doc(db,"rooms",roomId,"players",playerId),{ready:true});
@@ -27,3 +46,16 @@ onSnapshot(collection(db,"rooms",roomId,"players"), snap=>{
   });
   document.getElementById("players").innerHTML=html;
 });
+  snap.forEach(docSnap=>{
+    const p = docSnap.data();
+    if(p.isBot){
+      setTimeout(()=>{
+        let point = hitungPoint(p.cards || []);
+        if(point < 6){
+          console.log(p.name+" ambil kartu");
+        } else {
+          console.log(p.name+" stop");
+        }
+      },1500);
+    }
+  });
