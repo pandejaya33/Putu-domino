fa1f2bf9b911c4b973f2020ec4cc3254c9c4cfc3
pandejaya33 <pandejaya33@gmail.com>
2026-01-28 01:02:06 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function randomRoom(){
+  return Math.random().toString(36).substring(2,7);
+}
+
+window.buatRoom = ()=>{
+  const name = document.getElementById("nameInput").value;
+  if(!name) return alert("Isi nama dulu");
+
+  const roomId = randomRoom();
+
+  localStorage.setItem("playerName",name);
+  localStorage.setItem("roomId",roomId);
+
+  window.location.href="room.html";
+};
+
+window.gabungRoom = ()=>{
+  const name = document.getElementById("nameInput").value;
+  const roomId = document.getElementById("roomInput").value;
+
+  if(!name || !roomId) return alert("Isi nama dan kode room");
+
+  localStorage.setItem("playerName",name);
+  localStorage.setItem("roomId",roomId);
+
+  window.location.href="room.html";
+};
