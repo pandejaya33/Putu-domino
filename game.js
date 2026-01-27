c42fcbea70f7aa5fcf6db5ef4ac0e71f768aac45
pandejaya33 <pandejaya33@gmail.com>
2026-01-28 00:12:30 +0900

    Update game.js

--- game.js
+++ game.js
@@ -3,14 +3,14 @@ export function buatDeck(){
 
   for(let i=0;i<=6;i++){
     for(let j=i;j<=6;j++){
-      deck.push([i, j]); // ⬅️ FORMAT BENAR
+      deck.push({left:i, right:j});
     }
   }
 
-  // acak kartu
-  for(let k = deck.length - 1; k > 0; k--){
-    let r = Math.floor(Math.random() * (k + 1));
-    [deck[k], deck[r]] = [deck[r], deck[k]];
+  // acak
+  for(let k=deck.length-1;k>0;k--){
+    let r=Math.floor(Math.random()*(k+1));
+    [deck[k],deck[r]]=[deck[r],deck[k]];
   }
 
   return deck;
