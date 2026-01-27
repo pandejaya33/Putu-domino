b3d3e05a1d7503f2df1cdd6cd5251db5b86ce1ef
pandejaya33 <pandejaya33@gmail.com>
2026-01-28 00:29:08 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,17 +1,13 @@
 export function buatDeck(){
-  let deck = [];
-
+  let deck=[];
   for(let i=0;i<=6;i++){
     for(let j=i;j<=6;j++){
-      deck.push({left:i, right:j});
+      deck.push({left:i,right:j});
     }
   }
-
-  // acak
   for(let k=deck.length-1;k>0;k--){
     let r=Math.floor(Math.random()*(k+1));
     [deck[k],deck[r]]=[deck[r],deck[k]];
   }
-
   return deck;
 }
