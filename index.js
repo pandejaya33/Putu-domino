0b99e68675246a6b2f24b8f928cc83275526d0fc
pandejaya33 <pandejaya33@gmail.com>
2026-01-28 08:19:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ function randomRoom(){
 }
 
 window.buatRoom = function(){
-  const name = document.getElementById("nameInput").value;
-  if(!name) return alert("Isi nama dulu");
+  const name = nameInput.value;
+  const mode = modeSelect.value;
+  if(!name) return alert("Isi nama");
 
   const roomId = randomRoom();
 
   localStorage.setItem("playerName",name);
   localStorage.setItem("roomId",roomId);
+  localStorage.setItem("mode",mode);
 
   window.location.href="room.html";
 };
 
 window.gabungRoom = function(){
-  const name = document.getElementById("nameInput").value;
-  const roomId = document.getElementById("roomInput").value;
-
-  if(!name || !roomId) return alert("Isi nama dan kode room");
+  const name = nameInput.value;
+  const roomId = roomInput.value;
+  if(!name || !roomId) return alert("Isi semua");
 
   localStorage.setItem("playerName",name);
   localStorage.setItem("roomId",roomId);
