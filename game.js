e8ad8f093f219657857fe3d53c4d7cd1a7f7bdaa
pandejaya33 <pandejaya33@gmail.com>
2026-01-28 21:23:03 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,10 @@
-export function buatDeck(){
-  let deck=[];
-  for(let i=0;i<=6;i++){
-    for(let j=i;j<=6;j++){
-      deck.push({left:i,right:j,open:false});
+export function buatDeck() {
+  let deck = [];
+  for (let i = 0; i <= 6; i++) {
+    for (let j = i; j <= 6; j++) {
+      deck.push({ left: i, right: j });
     }
   }
-  return deck.sort(()=>Math.random()-0.5);
-}
-
-export function hitungSpirit(cards){
-  let total=0;
-  cards.forEach(c=> total+=c.left+c.right);
-  return total%10;
-}
-
-export function semuaSelesaiSpirit(players){
-  return players.every(p=>p.cards.length>=2);
-}
-
-export function semuaSelesaiBrerong(players){
-  return players.every(p=>p.cards.length===4);
+  // Kocok kartu
+  return deck.sort(() => Math.random() - 0.5);
 }
