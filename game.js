a568e50659508a6afd93431505d60cbaa260e974
pandejaya33 <pandejaya33@gmail.com>
2026-01-29 23:56:31 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-// Fungsi mengocok kartu Domino
 export function buatDeck() {
     let deck = [];
     for (let i = 0; i <= 6; i++) {
@@ -9,19 +8,15 @@ export function buatDeck() {
     return deck.sort(() => Math.random() - 0.5);
 }
 
-// Fungsi membuat Room Baru
 export function buatRoom() {
-    const id = Math.random().toString(36).substring(2, 7); // Kode unik 5 karakter
+    const id = Math.random().toString(36).substring(2, 7).toUpperCase();
     const pId = "p_" + Math.random().toString(36).substring(2, 6);
     localStorage.setItem("roomId", id);
     localStorage.setItem("playerId", pId);
     window.location.href = "room.html"; 
 }
 
-// Fungsi bergabung ke Room yang sudah ada
 export function gabungRoom() {
-    const id = localStorage.getItem("roomId");
-    if (!id) return alert("Masukkan kode room terlebih dahulu!");
     const pId = "p_" + Math.random().toString(36).substring(2, 6);
     localStorage.setItem("playerId", pId);
     window.location.href = "room.html";
