2a195ff446567a19876ef6a610133f42523c80c5
pandejaya33 <pandejaya33@gmail.com>
2026-01-29 23:34:53 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
 export function buatDeck() {
-  let deck = [];
-  for (let i = 0; i <= 6; i++) {
-    for (let j = i; j <= 6; j++) {
-      deck.push({ left: i, right: j });
+    let deck = [];
+    for (let i = 0; i < 7; i++) {
+        for (let j = i; j < 7; j++) {
+            deck.push({ left: i, right: j });
+        }
     }
-  }
-  // Kocok kartu
-  return deck.sort(() => Math.random() - 0.5);
+    return deck.sort(() => Math.random() - 0.5);
+}
+
+export function buatRoom() {
+    const id = Math.random().toString(36).substring(2, 7);
+    localStorage.setItem("roomId", id);
+    localStorage.setItem("playerId", "p_" + Math.random().toString(36).substring(2, 5));
+    window.location.href = "room.html"; // Pastikan nama file tujuan benar
+}
+
+export function gabungRoom() {
+    const id = localStorage.getItem("roomId");
+    if (id) {
+        localStorage.setItem("playerId", "p_" + Math.random().toString(36).substring(2, 5));
+        window.location.href = "room.html";
+    }
 }
